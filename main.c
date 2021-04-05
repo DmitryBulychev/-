fc65c89ca18899bedaadc2fedd76dbfa4f927ce0
DmitryBulychev <81636546+DmitryBulychev@users.noreply.github.com>
2021-04-05 17:54:14 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,15 +1,30 @@
 #include <stdio.h>
-
+#include <math.h>
 int main() {
-   double x, y;
-    printf("Введите координату x >");
-    scanf("%lf",&x);
-    printf("Введите координату y >");
-    scanf("%lf",&y);
-    printf("x=%6.3lf;  y=%6.3lf\n",x,y);
-    if((y<=0&&x<=1&&x>=-1)||(y<=1&&y>=-1&&x<=0)){
-        printf("Введенная точка попадает в область\n");
+    double a, b;//a=-1.3, b= 5.5
+    double y;
+    double m;
+    printf("Введите переменную а = ");
+    scanf("%lf", &a);
+    printf("Введите переменную b = ");
+    scanf("%lf", &b);
+    if (a<b){
+        m = (2*a*b)/(1+log(b));
     }
-    else printf("Введенная точка не попадает в область\n");
+    else
+    { m= tan(a)-1/sin(b);
+    }
+/* */
+    if (m<a){
+        y= log(a*m-b);
+    }
+    else if(a==m){
+        y=pow(cos(a),2)-sin(m*b);
+    }
+    else if (m>a){
+        y= (b+m)/(1-pow(cos(a), 2));
+    }
+    printf("m равно %.3lf\ny равно %.3lf", m, y);
+
     return 0;
 }
